6c45d516bc6f362e73b40a72ff5f22f120b90cbc
adityawintech <aditya#19AI>
2025-01-06 11:32:54 +0530

    Aditya

--- pages/register.js
+++ pages/register.js
@@ -1,3 +1,9 @@
 
 
-export const Register="<h1>Register</h1>";
+export const Register=`
+<div class="grid md:grid-cols-3 bg-red-200">
+ <div class="p-4 bg-green-200">a</div>
+ <div class="p-4 bg-green-400">b</div>
+ <div class="p-4 bg-green-500">c</div>
+<div>
+`;
