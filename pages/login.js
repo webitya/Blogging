22fc905dc1b5f55b1527e478282d229707029723
adityawintech <aditya#19AI>
2025-01-06 12:21:07 +0530

    Aditya

--- pages/login.js
+++ pages/login.js
@@ -1,1 +1,32 @@
-export const Login="<h1>Login</h1>";
+
+
+
+export const Login=`
+<div class="min-h-[400px] select-none md:pt-10">
+<div class="grid md:grid-cols-3">
+ <div class="p-4 md:border-r-4  md:border-blue-400"></div>
+ <div class="p-4 flex flex-col gap-4 shadow-lg rounded-sm">
+      <h1 class="bg-blue-500 text-white p-2">Login Form</h1>
+      <form class="p-2">
+
+      <div>
+      <label for name="email">E-mail</label>
+      <input type="email" class="w-full"/>
+      </div>
+
+      <div>
+      <label for name="mobile">Mobile</label>
+      <input type="number" class="w-full"/>
+      <div>
+
+      <div>
+      <label for name="password">Password</label>
+      <input type="password" class="w-full"/>
+      <div>
+      <button class="bg-blue-500 text-white w-full my-3 p-2">SUBMIT</button>
+      </form>
+ </div>
+ <div class="p-4"></div>
+<div>
+</div>
+`;
--- pages/register.js
+++ pages/register.js
@@ -1,9 +1,36 @@
 
 
+
 export const Register=`
-<div class="grid md:grid-cols-3 bg-red-200">
- <div class="p-4 bg-green-200">a</div>
- <div class="p-4 bg-green-400">b</div>
- <div class="p-4 bg-green-500">c</div>
+<div class="min-h-[400px] select-none md:pt-10">
+<div class="grid md:grid-cols-3">
+ <div class="p-4 md:border-r-4  md:border-blue-400"></div>
+ <div class="p-4 flex flex-col gap-4 shadow-lg rounded-sm">
+      <h1 class="bg-blue-500 text-white p-2">Registration Form</h1>
+      <form class="p-2">
+      <div>
+      <label for name="name">Name</label>
+      <input type="text" class="w-full"/>
+      </div>
+
+      <div>
+      <label for name="email">E-mail</label>
+      <input type="email" class="w-full"/>
+      </div>
+
+      <div>
+      <label for name="mobile">Mobile</label>
+      <input type="number" class="w-full"/>
+      <div>
+
+      <div>
+      <label for name="password">Password</label>
+      <input type="password" class="w-full"/>
+      <div>
+      <button class="bg-blue-500 text-white w-full my-3 p-2">SUBMIT</button>
+      </form>
+ </div>
+ <div class="p-4"></div>
 <div>
+</div>
 `;
